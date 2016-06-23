ad452122e62a2627eee1ace72b3023feff0ea884
wangenyong <wangenyong@gmail.com>
2016-06-23 21:24:34 +0800

    显示用户信心

--- js/components/SettingView.js
+++ js/components/SettingView.js
@@ -9,6 +9,7 @@ import React, { Component, PropTypes } from 'react';
 import Header from '../common/Header';
 import styleUtils from '../common/styleUtils';
 import Colors from '../common/Colors';
+import { connect } from 'react-redux';
 import {
   View,
   Text,
@@ -18,11 +19,21 @@ import {
   TextInput
 } from 'react-native';
 
-class SettingView extends Component {
-
-_logout() {
-
+type Props = {
+  realname: string;
+  username: string;
 }
+
+class SettingView extends Component {
+  props: Props;
+
+  constructor(props: Props) {
+    super(props);
+  }
+
+  _logout() {
+
+  }
 
   render() {
     var backgroundColor = Colors.colorPrimary;
@@ -31,11 +42,16 @@ _logout() {
         <Header
           title="系统设置"
           style={[{backgroundColor}]} />
-          <TouchableOpacity onPress={this._logout.bind(this)} >
-            <View style={styles.row}>
-              <Text style={styles.rowTitle} >注销</Text>
-            </View>
-          </TouchableOpacity>
+
+        <Text style={{margin: 10, fontSize: 16}} >华夏中专OA手机端 </Text>
+        <Text style={{marginLeft: 10, fontSize: 12}} >当前版本：0.3.5</Text>
+        <Text style={{margin: 10, fontSize: 16}} >当前用户：{this.props.realname}/{this.props.username}</Text>
+
+        <TouchableOpacity onPress={this._logout.bind(this)} >
+          <View style={styles.row}>
+            <Text style={styles.rowTitle} >注销</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     )
   }
@@ -65,4 +81,11 @@ const styles = StyleSheet.create({
   }
 })
 
-export default SettingView
+const mapStateToProps = (state) => {
+  return {
+    realname: state.user.realname,
+    username: state.user.username,
+  }
+}
+
+export default connect(mapStateToProps)(SettingView)
